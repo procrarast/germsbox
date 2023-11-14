33686553355134bdc67f22ab279064f513d0d099
procrarast <procrarast@gmail.com>
2023-11-14 16:57:57 -0600

    Remove animation feature as it's impossible in v3

--- content.js
+++ content.js
@@ -4,10 +4,18 @@ document.addEventListener('keydown', function () {
     console.log("Keydown recieved");
     if (event.key === "0") {
         console.log("Setting animation delay to zero");
-        chrome.runtime.sendMessage({action: "setDelay"});
+        setDelay();
     }
     if (event.key === "a") {
         console.log("Sending chrome api message");
-        chrome.runtime.sendMessage({action: "switchTabs"});
+        chrome.runtime.sendMessage({ action: "switchTabs" });
     }
 });
+
+function setDelay() {
+    if (typeof window.changeSetting === 'function') {
+        window.changeSetting('animationDelay', 4);
+    } else {
+        console.error("changeSetting function not found");
+    }
+}
