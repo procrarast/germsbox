dcafef8c0c9d9eacdc99c8b0fbd62de33fbadd55
procrarast <procrarast@gmail.com>
2023-11-15 10:54:58 -0600

    Ported to chrome.windows from chrome.tabs

--- background.js
+++ background.js
@@ -1,60 +1,59 @@
 console.log("Loading background.js");
 
-function setDelay() {
-	window.changeSetting('animationDelay', 4);
-}
-
-function switchTabs() {
-	console.log("Switching tabs");
-	
-	getGermsTabs(function (tabs) {
-		if (tabs) {
-			getActiveTabId(function (currentTabId) {
-				const nextTabId = getNextTabId(tabs, currentTabId);
-				setActiveTab(nextTabId);
-			});
-		} else {
-			// TODO: Perhaps open up a new tab or set a config to do so
-			console.log("Not enough tabs found.");
-		}
-	});
-
-}
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request.action === "setDelay") {
-		setDelay();
-	}
-	
-	if (request.action === "switchTabs") {
-        switchTabs();
+chrome.runtime.onMessage.addListener(function(request/*, sender, sendResponse*/) {
+	if (request.action === "switchWindows") {
+        switchWindows();
     }
 });
 
+function switchWindows() {
+    console.log("Switching windows");
+
+    getGermsWindows(function (windows) {
+        if (windows) {
+            getActiveWindowId(function (currentWindowId) {
+                const nextWindowId = getNextWindowId(windows, currentWindowId);
+                setActiveWindow(nextWindowId);
+            });
+        } else {
+            console.log("Not enough windows found.");
+        }
+    });
+}
+
 /* Get array of tabs with a certain URL if there are two of them. Return false otherwise. */
-function getGermsTabs(callback) {
-	chrome.tabs.query({url: "https://germs.io/*"}, function (tabs) {	
-		if (tabs.length === 2) {
-			callback(tabs);
-		} else {
-			callback(false);
-		}
-	});
+function getGermsWindows(callback) {
+    chrome.windows.getAll({populate: true}, function (windows) {
+        let germsWindows = windows.filter(win => 
+            win.tabs.some(tab => tab.url && tab.url.includes('https://germs.io')));
+
+        if (germsWindows.length === 2) {
+            callback(germsWindows);
+        } else {
+            callback(false);
+        }
+    });
 }
 
+
+function openGermsWindow() {
+	//TODO: Make it open a window of the same party URLa
+	console.log("Opening new window");
+	chrome.windows.create({url: "https://germs.io/"});
+}
 /* Get the name of the currently active tab */
-function getActiveTabId(callback) {
-	chrome.tabs.query({active:true, currentWindow: true}, function(currentTab) {
-		callback(currentTab[0].id);
-	})
+function getActiveWindowId(callback) {
+    chrome.windows.getCurrent({}, function(currentWindow) {
+        callback(currentWindow.id);
+    });
 }
 
-/* Get the tab to be switched to */
-function getNextTabId(tabs, currentTabId) {
-	return tabs[0].id === currentTabId ? tabs[1].id : tabs[0].id;
+/* Get the window to be switched to */
+function getNextWindowId(windows, currentWindowId) {
+    return windows[0].id === currentWindowId ? windows[1].id : windows[0].id;
 }
 
-function setActiveTab(nextTabId) {
-	chrome.tabs.update(nextTabId, {active: true});
+function setActiveWindow(nextWindowId) {
+    chrome.windows.update(nextWindowId, {focused: true});
 }
 
--- content.js
+++ content.js
@@ -2,20 +2,8 @@ console.log("Running content.js");
 
 document.addEventListener('keydown', function () {
     console.log("Keydown recieved");
-    if (event.key === "0") {
-        console.log("Setting animation delay to zero");
-        setDelay();
-    }
     if (event.key === "a") {
         console.log("Sending chrome api message");
-        chrome.runtime.sendMessage({ action: "switchTabs" });
+        chrome.runtime.sendMessage({ action: "switchWindows" });
     }
 });
-
-function setDelay() {
-    if (typeof window.changeSetting === 'function') {
-        window.changeSetting('animationDelay', 4);
-    } else {
-        console.error("changeSetting function not found");
-    }
-}
