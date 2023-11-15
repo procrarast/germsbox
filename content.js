98cde3cea5220efaa6d6b150f9abe4d72b0db5cc
procrarast <procrarast@gmail.com>
2023-11-15 13:01:39 -0600

    Small change

--- content.js
+++ content.js
@@ -4,11 +4,6 @@ document.addEventListener('keydown', function (event) {
     console.log("Keydown recieved");
     if (event.key === "a") {
         console.log("Sending chrome api message");
-        let response = await (chrome.runtime.sendMessage({ action: "switchTabs" }));
-        if (response) {
-            console.log(response);
-        } else {
-            console.log("There was a problem switching tabs.");
-        }
+        chrome.runtime.sendMessage({ action: "switchTabs" });
     }
 });
